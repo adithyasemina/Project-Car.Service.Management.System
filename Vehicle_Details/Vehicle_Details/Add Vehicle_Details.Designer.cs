58089a1eef97dce6c90e562c269177c929e32772
prabhabuddhini <137307140+prabhabuddhini@users.noreply.github.com>
2024-04-09 21:12:59 +0530

    Add files via upload

---
+++ Vehicle_Details/Vehicle_Details/Add Vehicle_Details.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace Vehicle_Details
+{
+    partial class Add_Vehicle_Details
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Add_Vehicle_Details));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblAddVehicle = new System.Windows.Forms.Label();
+            this.lblCName1 = new System.Windows.Forms.Label();
+            this.lblCNum1 = new System.Windows.Forms.Label();
+            this.lblVName1 = new System.Windows.Forms.Label();
+            this.lblVBrand1 = new System.Windows.Forms.Label();
+            this.lblVNum1 = new System.Windows.Forms.Label();
+            this.lblChassis1 = new System.Windows.Forms.Label();
+            this.lblFType1 = new System.Windows.Forms.Label();
+            this.lblEType1 = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.txtCT1 = new System.Windows.Forms.TextBox();
+            this.txtVNum1 = new System.Windows.Forms.TextBox();
+            this.txtVB1 = new System.Windows.Forms.TextBox();
+            this.txtVN1 = new System.Windows.Forms.TextBox();
+            this.txtCN1 = new System.Windows.Forms.TextBox();
+            this.txtCNum1 = new System.Windows.Forms.TextBox();
+            this.txtFT1 = new System.Windows.Forms.TextBox();
+            this.txtET1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRef = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.lblAddVehicle);
+            this.panel1.Location = new System.Drawing.Point(1, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1419, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(126, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblAddVehicle
+            // 
+            this.lblAddVehicle.AutoSize = true;
+            this.lblAddVehicle.Font = new System.Drawing.Font("Stencil", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddVehicle.Location = new System.Drawing.Point(336, 22);
+            this.lblAddVehicle.Name = "lblAddVehicle";
+            this.lblAddVehicle.Size = new System.Drawing.Size(453, 47);
+            this.lblAddVehicle.TabIndex = 0;
+            this.lblAddVehicle.Text = "ADD VEHICLE DETAILS";
+            // 
+            // lblCName1
+            // 
+            this.lblCName1.AutoSize = true;
+            this.lblCName1.BackColor = System.Drawing.SystemColors.Control;
+            this.lblCName1.Font = new System.Drawing.Font("Rockwell Nova", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCName1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblCName1.Location = new System.Drawing.Point(51, 156);
+            this.lblCName1.Name = "lblCName1";
+            this.lblCName1.Size = new System.Drawing.Size(199, 27);
+            this.lblCName1.TabIndex = 15;
+            this.lblCName1.Text = "CUSTOMER NAME";
+            // 
+            // lblCNum1
+            // 
+            this.lblCNum1.AutoSize = true;
+            this.lblCNum1.Font = new System.Drawing.Font("Rockwell Nova", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCNum1.Location = new System.Drawing.Point(51, 367);
+            this.lblCNum1.Name = "lblCNum1";
+            this.lblCNum1.Size = new System.Drawing.Size(267, 27);
+            this.lblCNum1.TabIndex = 16;
+            this.lblCNum1.Text = "CUSTOMER TEL.NUMBER";
+            // 
+            // lblVName1
+            // 
+            this.lblVName1.AutoSize = true;
+            this.lblVName1.Font = new System.Drawing.Font("Rockwell Nova", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVName1.Location = new System.Drawing.Point(51, 209);
+            this.lblVName1.Name = "lblVName1";
+            this.lblVName1.Size = new System.Drawing.Size(173, 27);
+            this.lblVName1.TabIndex = 17;
+            this.lblVName1.Text = "VEHICLE NAME";
+            // 
+            // lblVBrand1
+            // 
+            this.lblVBrand1.AutoSize = true;
+            this.lblVBrand1.Font = new System.Drawing.Font("Rockwell Nova", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVBrand1.Location = new System.Drawing.Point(51, 261);
+            this.lblVBrand1.Name = "lblVBrand1";
+            this.lblVBrand1.Size = new System.Drawing.Size(182, 27);
+            this.lblVBrand1.TabIndex = 18;
+            this.lblVBrand1.Text = "VEHICLE BRAND";
+            // 
+            // lblVNum1
+            // 
+            this.lblVNum1.AutoSize = true;
+            this.lblVNum1.Font = new System.Drawing.Font("Rockwell Nova", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVNum1.Location = new System.Drawing.Point(51, 313);
+            this.lblVNum1.Name = "lblVNum1";
+            this.lblVNum1.Size = new System.Drawing.Size(197, 27);
+            this.lblVNum1.TabIndex = 19;
+            this.lblVNum1.Text = "VEHICLE NUMBER";
+            // 
+            // lblChassis1
+            // 
+            this.lblChassis1.AutoSize = true;
+            this.lblChassis1.Font = new System.Drawing.Font("Rockwell Nova", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChassis1.Location = new System.Drawing.Point(51, 421);
+            this.lblChassis1.Name = "lblChassis1";
+            this.lblChassis1.Size = new System.Drawing.Size(195, 27);
+            this.lblChassis1.TabIndex = 20;
+            this.lblChassis1.Text = "CHASSIS NUMBER";
+            // 
+            // lblFType1
+            // 
+            this.lblFType1.AutoSize = true;
+            this.lblFType1.Font = new System.Drawing.Font("Rockwell Nova", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFType1.Location = new System.Drawing.Point(51, 472);
+            this.lblFType1.Name = "lblFType1";
+            this.lblFType1.Size = new System.Drawing.Size(122, 27);
+            this.lblFType1.TabIndex = 21;
+            this.lblFType1.Text = "FUEL TYPE";
+            // 
+            // lblEType1
+            // 
+            this.lblEType1.AutoSize = true;
+            this.lblEType1.Font = new System.Drawing.Font("Rockwell Nova", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEType1.Location = new System.Drawing.Point(51, 524);
+            this.lblEType1.Name = "lblEType1";
+            this.lblEType1.Size = new System.Drawing.Size(152, 27);
+            this.lblEType1.TabIndex = 22;
+            this.lblEType1.Text = "ENGINE TYPE";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Rockwell Nova", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(51, 578);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(70, 27);
+            this.lblDate.TabIndex = 23;
+            this.lblDate.Text = "DATE";
+            // 
+            // txtCT1
+            // 
+            this.txtCT1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCT1.Location = new System.Drawing.Point(354, 365);
+            this.txtCT1.Name = "txtCT1";
+            this.txtCT1.Size = new System.Drawing.Size(754, 30);
+            this.txtCT1.TabIndex = 24;
+            // 
+            // txtVNum1
+            // 
+            this.txtVNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVNum1.Location = new System.Drawing.Point(354, 315);
+            this.txtVNum1.Name = "txtVNum1";
+            this.txtVNum1.Size = new System.Drawing.Size(754, 30);
+            this.txtVNum1.TabIndex = 25;
+            // 
+            // txtVB1
+            // 
+            this.txtVB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVB1.Location = new System.Drawing.Point(354, 258);
+            this.txtVB1.Name = "txtVB1";
+            this.txtVB1.Size = new System.Drawing.Size(754, 30);
+            this.txtVB1.TabIndex = 26;
+            // 
+            // txtVN1
+            // 
+            this.txtVN1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVN1.Location = new System.Drawing.Point(354, 206);
+            this.txtVN1.Name = "txtVN1";
+            this.txtVN1.Size = new System.Drawing.Size(754, 30);
+            this.txtVN1.TabIndex = 27;
+            // 
+            // txtCN1
+            // 
+            this.txtCN1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCN1.Location = new System.Drawing.Point(354, 153);
+            this.txtCN1.Name = "txtCN1";
+            this.txtCN1.Size = new System.Drawing.Size(754, 30);
+            this.txtCN1.TabIndex = 2;
+            // 
+            // txtCNum1
+            // 
+            this.txtCNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCNum1.Location = new System.Drawing.Point(354, 418);
+            this.txtCNum1.Name = "txtCNum1";
+            this.txtCNum1.Size = new System.Drawing.Size(754, 30);
+            this.txtCNum1.TabIndex = 28;
+            // 
+            // txtFT1
+            // 
+            this.txtFT1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFT1.Location = new System.Drawing.Point(354, 472);
+            this.txtFT1.Name = "txtFT1";
+            this.txtFT1.Size = new System.Drawing.Size(754, 30);
+            this.txtFT1.TabIndex = 29;
+            // 
+            // txtET1
+            // 
+            this.txtET1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtET1.Location = new System.Drawing.Point(354, 524);
+            this.txtET1.Name = "txtET1";
+            this.txtET1.Size = new System.Drawing.Size(754, 30);
+            this.txtET1.TabIndex = 30;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1226, 195);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(131, 93);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 32;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnSave.Font = new System.Drawing.Font("Rockwell Nova", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSave.Location = new System.Drawing.Point(1226, 336);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(130, 39);
+            this.btnSave.TabIndex = 33;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRef
+            // 
+            this.btnRef.BackColor = System.Drawing.Color.DarkGray;
+            this.btnRef.Font = new System.Drawing.Font("Rockwell Nova", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRef.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnRef.Location = new System.Drawing.Point(1226, 398);
+            this.btnRef.Name = "btnRef";
+            this.btnRef.Size = new System.Drawing.Size(130, 45);
+            this.btnRef.TabIndex = 34;
+            this.btnRef.Text = "REFRESH";
+            this.btnRef.UseVisualStyleBackColor = false;
+            this.btnRef.Click += new System.EventHandler(this.btnRef_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.LightGray;
+            this.btnExit.Font = new System.Drawing.Font("Rockwell Nova", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(1226, 463);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(131, 39);
+            this.btnExit.TabIndex = 35;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(354, 579);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(754, 26);
+            this.dateTimePicker1.TabIndex = 36;
+            this.dateTimePicker1.Value = new System.DateTime(2024, 4, 9, 0, 0, 0, 0);
+            // 
+            // Add_Vehicle_Details
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1443, 712);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRef);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.txtET1);
+            this.Controls.Add(this.txtFT1);
+            this.Controls.Add(this.txtCNum1);
+            this.Controls.Add(this.txtCN1);
+            this.Controls.Add(this.txtVN1);
+            this.Controls.Add(this.txtVB1);
+            this.Controls.Add(this.txtVNum1);
+            this.Controls.Add(this.txtCT1);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblEType1);
+            this.Controls.Add(this.lblFType1);
+            this.Controls.Add(this.lblChassis1);
+            this.Controls.Add(this.lblVNum1);
+            this.Controls.Add(this.lblVBrand1);
+            this.Controls.Add(this.lblVName1);
+            this.Controls.Add(this.lblCNum1);
+            this.Controls.Add(this.lblCName1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Add_Vehicle_Details";
+            this.Text = "Add_Vehicle_Details";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblAddVehicle;
+        private System.Windows.Forms.Label lblCName1;
+        private System.Windows.Forms.Label lblCNum1;
+        private System.Windows.Forms.Label lblVName1;
+        private System.Windows.Forms.Label lblVBrand1;
+        private System.Windows.Forms.Label lblVNum1;
+        private System.Windows.Forms.Label lblChassis1;
+        private System.Windows.Forms.Label lblFType1;
+        private System.Windows.Forms.Label lblEType1;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.TextBox txtCT1;
+        private System.Windows.Forms.TextBox txtVNum1;
+        private System.Windows.Forms.TextBox txtVB1;
+        private System.Windows.Forms.TextBox txtVN1;
+        private System.Windows.Forms.TextBox txtCN1;
+        private System.Windows.Forms.TextBox txtCNum1;
+        private System.Windows.Forms.TextBox txtFT1;
+        private System.Windows.Forms.TextBox txtET1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRef;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
