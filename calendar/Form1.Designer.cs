db0bceaf71cd398325a3389303220bbb4bc0c341
Venuraka Ranasinghe <venurakaranasinghe@gmail.com>
2024-04-10 18:03:50 +0530

    made full calendar

--- calendar/Form1.Designer.cs
+++ calendar/Form1.Designer.cs
@@ -38,102 +38,116 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.LBDATE = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // daycontainer
             // 
-            this.daycontainer.Location = new System.Drawing.Point(13, 179);
+            this.daycontainer.Location = new System.Drawing.Point(12, 179);
             this.daycontainer.Name = "daycontainer";
-            this.daycontainer.Size = new System.Drawing.Size(1141, 643);
+            this.daycontainer.Size = new System.Drawing.Size(1142, 547);
             this.daycontainer.TabIndex = 0;
             // 
             // btnnext
             // 
-            this.btnnext.Location = new System.Drawing.Point(1038, 872);
+            this.btnnext.Location = new System.Drawing.Point(1038, 752);
             this.btnnext.Name = "btnnext";
             this.btnnext.Size = new System.Drawing.Size(75, 23);
             this.btnnext.TabIndex = 0;
             this.btnnext.Text = "Next";
             this.btnnext.UseVisualStyleBackColor = true;
+            this.btnnext.Click += new System.EventHandler(this.btnnext_Click);
             // 
             // btnprevious
             // 
-            this.btnprevious.Location = new System.Drawing.Point(936, 872);
+            this.btnprevious.Location = new System.Drawing.Point(957, 752);
             this.btnprevious.Name = "btnprevious";
             this.btnprevious.Size = new System.Drawing.Size(75, 23);
             this.btnprevious.TabIndex = 1;
             this.btnprevious.Text = "Previous";
             this.btnprevious.UseVisualStyleBackColor = true;
+            this.btnprevious.Click += new System.EventHandler(this.btnprevious_Click);
             // 
             // label1
             // 
-            this.label1.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Poppins Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(48, 126);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 28);
+            this.label1.Size = new System.Drawing.Size(85, 28);
             this.label1.TabIndex = 0;
             this.label1.Text = "Monday";
             // 
             // label2
             // 
-            this.label2.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Poppins Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(213, 126);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 28);
+            this.label2.Size = new System.Drawing.Size(85, 28);
             this.label2.TabIndex = 2;
             this.label2.Text = "Tuesday";
             // 
             // label3
             // 
-            this.label3.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(367, 126);
+            this.label3.Font = new System.Drawing.Font("Poppins Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(361, 126);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(98, 28);
+            this.label3.Size = new System.Drawing.Size(116, 28);
             this.label3.TabIndex = 3;
             this.label3.Text = "Wednesday";
             // 
             // label4
             // 
-            this.label4.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(540, 126);
+            this.label4.Font = new System.Drawing.Font("Poppins Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(534, 126);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(75, 28);
+            this.label4.Size = new System.Drawing.Size(95, 28);
             this.label4.TabIndex = 4;
             this.label4.Text = "Thusrday";
             // 
             // label5
             // 
-            this.label5.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(718, 126);
+            this.label5.Font = new System.Drawing.Font("Poppins Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(702, 126);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(53, 28);
+            this.label5.Size = new System.Drawing.Size(68, 28);
             this.label5.TabIndex = 5;
             this.label5.Text = "Friday";
             // 
             // label6
             // 
-            this.label6.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(863, 126);
+            this.label6.Font = new System.Drawing.Font("Poppins Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(851, 126);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(77, 28);
+            this.label6.Size = new System.Drawing.Size(92, 28);
             this.label6.TabIndex = 6;
             this.label6.Text = "Saturday";
             // 
             // label7
             // 
-            this.label7.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(1034, 126);
+            this.label7.Font = new System.Drawing.Font("Poppins Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(1018, 126);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(70, 28);
+            this.label7.Size = new System.Drawing.Size(85, 28);
             this.label7.TabIndex = 7;
             this.label7.Text = "Sunday";
+            // 
+            // LBDATE
+            // 
+            this.LBDATE.Font = new System.Drawing.Font("Poppins Black", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBDATE.Location = new System.Drawing.Point(316, 28);
+            this.LBDATE.Name = "LBDATE";
+            this.LBDATE.Size = new System.Drawing.Size(554, 50);
+            this.LBDATE.TabIndex = 8;
+            this.LBDATE.Text = "MONTH YEAR";
+            this.LBDATE.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
-            this.ClientSize = new System.Drawing.Size(1165, 927);
+            this.ClientSize = new System.Drawing.Size(1165, 796);
+            this.Controls.Add(this.LBDATE);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -165,6 +179,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label LBDATE;
     }
 }
 
--- calendar/Form1.cs
+++ calendar/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace calendar
 {
     public partial class Form1 : Form
     {
+        int month, year;
         public Form1()
         {
             InitializeComponent();
@@ -24,15 +26,19 @@ namespace calendar
         private void displaydays()
         {
             DateTime now = DateTime.Now;
+            month = now.Month;
+            year = now.Year;
+            string monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            LBDATE.Text = monthname + "" + year;
 
             // Getting first day of the month
-            DateTime startofthemonth = new DateTime(now.Year,now.Month,1);
+            DateTime startofthemonth = new DateTime(year, month,1);
 
             // Getting the count of the days of the month
-            int days=DateTime.DaysInMonth(now.Year,now.Month);
+            int days=DateTime.DaysInMonth(year, month);
 
             //converting the start of the month in to the integer 
-            int daysoftheweek = Convert.ToInt32(startofthemonth.DayOfWeek.ToString("d"))+1;
+            int daysoftheweek = Convert.ToInt32(startofthemonth.DayOfWeek.ToString("d"));
 
             //Creating blank user control 
             for (int i = 1; i < daysoftheweek; i++)
@@ -41,7 +47,84 @@ namespace calendar
                 daycontainer.Controls.Add(ucblank);
             }
             //Creating user controll for days
+            for (int i = 1; i<=days; i++) 
+            {
+                UserControlDays ucdays = new UserControlDays();
+                ucdays.days(i);
+                daycontainer.Controls.Add(ucdays);
+            }   
+        }
 
+        private void btnprevious_Click(object sender, EventArgs e)
+        {
+            //clearing container
+            daycontainer.Controls.Clear();
+
+            //decrementing month to go to previous month
+            month--;
+
+            string monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            LBDATE.Text = monthname + "" + year;
+
+
+            // Getting first day of the month
+            DateTime startofthemonth = new DateTime(year, month, 1);
+
+            // Getting the count of the days of the month
+            int days = DateTime.DaysInMonth(year, month);
+
+            //converting the start of the month in to the integer 
+            int daysoftheweek = Convert.ToInt32(startofthemonth.DayOfWeek.ToString("d"));
+
+            //Creating blank user control 
+            for (int i = 1; i < daysoftheweek; i++)
+            {
+                UserControlBlank ucblank = new UserControlBlank();
+                daycontainer.Controls.Add(ucblank);
+            }
+            //Creating user controll for days
+            for (int i = 1; i <= days; i++)
+            {
+                UserControlDays ucdays = new UserControlDays();
+                ucdays.days(i);
+                daycontainer.Controls.Add(ucdays);
+            }
+        }
+
+        private void btnnext_Click(object sender, EventArgs e)
+        {
+            //clearing container
+            daycontainer.Controls.Clear();
+
+            //Incrementing month to go to next month
+            month++;
+
+            string monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            LBDATE.Text = monthname + "" + year;
+
+
+            // Getting first day of the month
+            DateTime startofthemonth = new DateTime(year, month, 1);
+
+            // Getting the count of the days of the month
+            int days = DateTime.DaysInMonth(year, month);
+
+            //converting the start of the month in to the integer 
+            int daysoftheweek = Convert.ToInt32(startofthemonth.DayOfWeek.ToString("d")); 
+
+            //Creating blank user control 
+            for (int i = 1; i < daysoftheweek; i++)
+            {
+                UserControlBlank ucblank = new UserControlBlank();
+                daycontainer.Controls.Add(ucblank);
+            }
+            //Creating user controll for days
+            for (int i = 1; i <= days; i++)
+            {
+                UserControlDays ucdays = new UserControlDays();
+                ucdays.days(i);
+                daycontainer.Controls.Add(ucdays);
+            }
         }
     }
 }
