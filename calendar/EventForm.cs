d43e74035980baee0d9b35a1c1beb8282d25962e
Venuraka Ranasinghe <venurakaranasinghe@gmail.com>
2024-04-20 19:25:15 +0530

    fixed bugs

--- calendar/EventForm.cs
+++ calendar/EventForm.cs
@@ -9,8 +9,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
-
 namespace calendar
 {
     public partial class EventForm : Form
@@ -23,20 +21,32 @@ namespace calendar
         private void EventForm_Load(object sender, EventArgs e)
         {
             //calling static variable we declared 
-            txdate.Text =Form1.static_month+"/"+ UserControlDays.static_day + "/" + Form1.static_year;
+            txdate.Text = Form1.static_month + "/" + UserControlDays.static_day + "/" + Form1.static_year;
         }
 
         private void btnsave_Click(object sender, EventArgs e)
         {
             //Creating a connectionstring
-            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\user\Documents\calendar.mdf;Integrated Security=True;Connect Timeout=30");
-            conn.Open();
-            SqlCommand cmd = new SqlCommand("insert into tbl_calendar values('"+txdate.Text+"','"+txvehicle.Text+"')",conn);
+            using (SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\user\Documents\calendar.mdf;Integrated Security=True;Connect Timeout=30"))
+            {
+                try
+                {
+                    conn.Open();
 
-            cmd.ExecuteNonQuery();
-            MessageBox.Show("saved","Alert");
-            cmd.Dispose();
-            conn.Close();
+                    // Create a parameterized query to prevent SQL injection
+                    string query = "INSERT INTO tbl_calendar (date, vehicle) VALUES (@date, @vehicle)";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@date", txdate.Text);
+                    cmd.Parameters.AddWithValue("@vehicle", txvehicle.Text);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Saved", "Alert");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Error");
+                }
+            }
         }
     }
 }
---
+++ dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tbl_calendar ]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [date] NVARCHAR(50) NOT NULL, 
+    [vehicle] NVARCHAR(50) NOT NULL
+)
